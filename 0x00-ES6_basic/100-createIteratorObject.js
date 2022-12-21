028c750bd00e65169b2338a9e1c518e40a394360
cyrusDev1 <cyrushoungue1240@gmail.com>
2022-12-21 18:23:50 +0100

    task 13 iterator object

---
+++ 0x00-ES6_basic/100-createIteratorObject.js
@@ -0,0 +1,10 @@
+export default function createIteratorObject(report) {
+  const arrReport = (Object.values(report.allEmployees));
+  const temp = [];
+  for (const list of arrReport) {
+    for (const value of list) {
+      temp.push(value);
+    }
+  }
+  return temp;
+}
