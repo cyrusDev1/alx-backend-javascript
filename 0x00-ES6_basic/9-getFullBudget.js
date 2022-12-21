25f298ee4497347afa85a7dce1c9172d73a75558
cyrusDev1 <cyrushoungue1240@gmail.com>
2022-12-21 15:44:31 +0100

    task 9 method es6

---
+++ 0x00-ES6_basic/9-getFullBudget.js
@@ -0,0 +1,17 @@
+// eslint-disable-next-line
+import getBudgetObject from './7-getBudgetObject.js';
+
+export default function getFullBudgetObject(income, gdp, capita) {
+  const budget = getBudgetObject(income, gdp, capita);
+  const fullBudget = {
+    ...budget,
+    getIncomeInDollars() {
+      return `$${this.income}`;
+    },
+    getIncomeInEuros() {
+      return `${this.income} euros`;
+    },
+  };
+
+  return fullBudget;
+}
