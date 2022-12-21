74ffd4b8c9cdcec7522f097a0cce1bef6013e14a
cyrusDev1 <cyrushoungue1240@gmail.com>
2022-12-21 18:30:04 +0100

    task 13 join

--- 0x00-ES6_basic/100-createIteratorObject.js
+++ 0x00-ES6_basic/100-createIteratorObject.js
@@ -1,5 +1,5 @@
 export default function createIteratorObject(report) {
-  const arrReport = (Object.values(report.allEmployees));
+  const arrReport = Object.values(report.allEmployees);
   const temp = [];
   for (const list of arrReport) {
     for (const value of list) {
