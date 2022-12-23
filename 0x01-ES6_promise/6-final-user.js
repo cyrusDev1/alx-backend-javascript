e516770d0645200d3acfe9bbc370a0361f05f1c0
cyrusDev1 <cyrushoungue1240@gmail.com>
2022-12-24 00:47:46 +0100

    task 6 multiple promise

---
+++ 0x01-ES6_promise/6-final-user.js
@@ -0,0 +1,17 @@
+import signUpUser from './4-user-promise';
+import uploadPhoto from './5-photo-reject';
+
+export default function handleProfileSignup(firstName, lastName, filename) {
+  return Promise.allSettled([signUpUser(firstName, lastName), uploadPhoto(filename)])
+    .then((results) => {
+      const array = [];
+      results.forEach((result) => {
+        if (result.status === 'fulfilled') {
+          array.push({ status: result.status, value: result.value });
+        } else if (result.status === 'rejected') {
+          array.push({ status: result.status, value: result.reason });
+        }
+      });
+      return array;
+    });
+}
