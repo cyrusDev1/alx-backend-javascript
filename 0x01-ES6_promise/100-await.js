74738c668a52b6b8d18af58243abdb53cb035b41
cyrusDev1 <cyrushoungue1240@gmail.com>
2022-12-24 12:26:09 +0100

    task 10 await

--- 0x01-ES6_promise/100-await.js
+++ 0x01-ES6_promise/100-await.js
@@ -1,12 +1,16 @@
 import { uploadPhoto, createUser } from './utils';
 
 export default async function asyncUploadUser() {
-  const val = await Promise.all([uploadPhoto(), createUser()])
-    .then((value) => ({
-      photo: value[0],
-      user: value[1],
-    })).catch((err) => {
-      console.log(err);
-    });
-  return val;
+  try {
+    return await Promise.all([uploadPhoto(), createUser()])
+      .then((value) => ({
+        photo: value[0],
+        user: value[1],
+      }));
+  } catch (error) {
+    return {
+      photo: null,
+      user: null,
+    };
+  }
 }
