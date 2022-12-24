4b8b956448d5a524948a249082adbe3ad9e60765
cyrusDev1 <cyrushoungue1240@gmail.com>
2022-12-24 15:25:19 +0100

    task 3 crrency

---
+++ 0x02-ES6_classes/3-currency.js
@@ -0,0 +1,36 @@
+/* eslint no-underscore-dangle: ["error", {"allow": ["_name", "_code"] }] */
+
+export default class HolbertonCourse {
+  constructor(code, name) {
+    this.code = code;
+    this.name = name;
+  }
+
+  get code() {
+    return this._code;
+  }
+
+  set code(code) {
+    if (typeof code === 'string') {
+      this._code = code;
+    } else {
+      throw new TypeError('Code must be a string');
+    }
+  }
+
+  get name() {
+    return this._name;
+  }
+
+  set name(name) {
+    if (typeof name === 'string') {
+      this._name = name;
+    } else {
+      throw new TypeError('Name must be a string');
+    }
+  }
+
+  displayFullCurrency() {
+    return `${this.name} (${this.code})`;
+  }
+}
