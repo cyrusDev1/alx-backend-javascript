2ff7892659746cc7b4e32a9e1f560b20975a17ed
cyrusDev1 <cyrushoungue1240@gmail.com>
2022-12-30 15:18:49 +0100

    task 11 evcar

---
+++ 0x02-ES6_classes/100-evcar.js
@@ -0,0 +1,14 @@
+/* eslint no-underscore-dangle: ["error", {"allow": ["_range", "_color", "_brand", "_motor"] }] */
+import Car from './10-car';
+
+export default class EVCar extends Car {
+  constructor(brand, motor, color, range) {
+    super(brand, motor, color);
+    this._range = range;
+  }
+
+  cloneCar() {
+    const Obj = super.constructor;
+    return new Obj();
+  }
+}
