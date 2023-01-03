b1ac167f50a680421cc3050e996929c65a9b23f1
cyrusDev1 <cyrushoungue1240@gmail.com>
2023-01-04 00:22:45 +0100

    task 100 weekmap

---
+++ 0x03-ES6_data_manipulation/100-weak.js
@@ -0,0 +1,13 @@
+const weakMap = new WeakMap();
+const queryAPI = (endpoint) => {
+  if (!weakMap.has(endpoint)) {
+    weakMap.set(endpoint, 0);
+  }
+  weakMap.set(endpoint, weakMap.get(endpoint) + 1);
+  if (weakMap.get(endpoint) >= 5) {
+    throw new Error('Endpoint load is high');
+  }
+};
+export {
+  weakMap, queryAPI,
+};
