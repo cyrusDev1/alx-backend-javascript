0f3c755211bcbf7ea46f773396ae8553dab46e9b
cyrusDev1 <cyrushoungue1240@gmail.com>
2023-01-03 23:11:11 +0100

    task 8 clean set

--- 0x03-ES6_data_manipulation/8-clean_set.js
+++ 0x03-ES6_data_manipulation/8-clean_set.js
@@ -1,6 +1,9 @@
 export default function cleanSet(set, startString) {
   if (startString === '' || typeof startString !== 'string') { return ''; }
-  return [...set].filter((el) => el.startsWith(startString)).map((el) => {
-    if (typeof el === 'string') return el.slice(startString.length);
+  return [...set].filter((el) => el.startsWith(startString)).map((ele) => {
+    if (typeof ele === 'string') {
+      return ele.slice(startString.length);
+    }
+    return false;
   }).join('-');
 }
