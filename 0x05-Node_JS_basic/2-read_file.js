a5ec0d3997460b25810265fed1e0ee351901732f
cyrusDev1 <cyrushoungue1240@gmail.com>
2023-02-20 15:52:49 +0100

    task 2 read file

---
+++ 0x05-Node_JS_basic/2-read_file.js
@@ -0,0 +1,26 @@
+const fs = require('fs');
+const readline = require('readline');
+function countStudents (path) {
+  const stream = fs.createReadStream(path);
+  const rl = readline.createInterface({ input: stream });
+  const data = [];
+  rl.on('line', (row) => {
+    data.push(row.split(','));
+  });
+  rl.on('close', () => {
+    console.log(`Number of students: ${data.length - 1}`);
+    const cs = [];
+    const swe = [];
+    data.forEach(element => {
+      if (element[element.length - 1] === 'CS') {
+        cs.push(element[0]);
+      } else if (element[element.length - 1] === 'SWE') {
+        swe.push(element[0]);
+      }
+    });
+    console.log(`Number of students in CS: ${cs.length}. List: ${cs.join(', ')}`);
+    console.log(`Number of students in SWS: ${swe.length}. List: ${swe.join(', ')}`);
+  });
+}
+
+module.exports = countStudents;
