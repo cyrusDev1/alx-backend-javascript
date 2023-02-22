900f60ab1ba0898757248201c3fe3de085c9d981
cyrusDev1 <cyrushoungue1240@gmail.com>
2023-02-22 10:11:59 +0100

    add files

--- 0x05-Node_JS_basic/1-stdin.js
+++ 0x05-Node_JS_basic/1-stdin.js
@@ -1,9 +1,13 @@
-const readline = require('readline').createInterface({
-  input: process.stdin,
-  output: process.stdout
+process.stdout.write('Welcome to Holberton School, what is your name?\n');
+
+process.stdin.on('readable', () => {
+  const name = process.stdin.read();
+
+  if (name) {
+    process.stdout.write(`Your name is: ${name}`);
+  }
 });
-console.log('Welcome to Holberton School, what is your name?');
-readline.question('Your name is: ', name => {
-  console.log('This important software is now closing');
-  readline.close();
+
+process.stdin.on('close', () => {
+  process.stdout.write('This important software is now closing\n');
 });
