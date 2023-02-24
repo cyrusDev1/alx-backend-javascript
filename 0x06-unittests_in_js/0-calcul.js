5f8dd0506e18990a3496e38b680ec4c310bceed8
cyrusDev1 <cyrushoungue1240@gmail.com>
2023-02-24 16:50:15 +0100

    task 0 add first test case with mocha

--- 0x06-unittests_in_js/0-calcul.js
+++ 0x06-unittests_in_js/0-calcul.js
@@ -1,5 +1,7 @@
 function calculateNumber (a, b) {
-  return Math.round(b) + Math.round(a);
+    if (Number.isNaN(Number(a)) || Number.isNaN(Number(b)))
+        throw TypeError('Parameters must be numbers');
+  return Math.round(Number(a)) + Math.round(Number(b));
 }
 
 module.exports = calculateNumber;
